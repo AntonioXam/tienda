6a6eae97816f524290856cb60a4c77827f069e68
AntonioXam <157708024+AntonioXam@users.noreply.github.com>
2024-12-19 10:18:01 +0100

    Agregar funcionalidad de gestión de productos: listado, creación, edición y eliminación

---
+++ laravel/resources/views/products/edit.blade.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Editar producto
+                        <a  href="{{ route ('products.index') }}" class="btn btn-primary btn-sm float-end">Volver</a>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route ('products.update', $product->id) }}" method="POST">
+                            @method('put')
+                            @csrf
+                            <div class="mb-3">
+                                <label for="description" class="form-label">Descripción</label>
+                                <textarea class="form-control" id="description" name="description">{{$product->description}}</textarea>
+                            </div>
+                            <div class="mb-3">
+                                <label for="price" class="form-label">Precio</label>
+                                <input type="number" step="0.01" class="form-control" id="price" name="price" value="{{$product->price}}">
+                            </div>
+                            <button type="submit" class="btn btn-primary">Guardar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    
+
+
+</body>
+</html>
--- laravel/resources/views/products/index.blade.php
+++ laravel/resources/views/products/index.blade.php
@@ -7,7 +7,48 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>Listado de productos</h1>
+    <div class="container mt-3">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">
+                        Listado de productos
+                        <a  href="{{ route ('products.create') }}" class="btn btn-success btn-sm float-end">Nuevo producto</a>
+                    </div>
+                    <div class="card-body">
+                        @if(session('info'))
+                        <div class="alert alert-success">
+                            {{session('info')}}
+                        </div>
+                        @endif
+                        <table class="table table-hover table-sm">
+                            <thead>
+                                    <th>Descripción</th>
+                                    <th>Precio</th>
+                                    <th>Acciones</th>
+                            </thead>
+                            <tbody>
+                                @foreach($products as $product)
+                                <tr>
+                                    <td>{{$product->description}}</td>
+                                    <td>{{$product->price}}</td>
+                                    <td>
+                                        <a href="{{ route ('products.edit', $product->id) }}" class="btn btn-warning btn-sm">Editar</a>
+                                        <form action="{{ route ('products.destroy', $product->id) }}" method="POST" style="display: inline">
+                                            @method('delete')
+                                            @csrf
+                                            <button type="submit" class="btn btn-danger btn-sm">Eliminar</button>
+                                        </form>
+                                    </td>
+                                </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
